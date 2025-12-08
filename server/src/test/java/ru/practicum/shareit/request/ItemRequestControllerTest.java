e24ade4d7f309ac3a2129e29d4c83c5573fdb233
Yedige7 <Yedige7@gmail.com>
2025-12-08 19:35:39 +0500

    Добавил правки по чеку стилю 4.1

--- server/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -13,7 +13,6 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
